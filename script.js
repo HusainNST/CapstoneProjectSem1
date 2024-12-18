5aa3d4f7f55c44cec6c9981b21e4c3bf0f6ca6a4
Husain Khorakiwala <hussainkhorakiwala45@gmail.com>
2024-12-18 13:52:13 +0530

    Finished mobile reponsiveness

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ function type() {
 }
 
 type();
+
+// For toggling the navbar elements for responsiveness
+function toggleButton() {
+  const nav = document.querySelector(".pageLinks");
+  nav.classList.toggle("hidden");
+
+  const socials = document.querySelector(".socials");
+  socials.classList.toggle("hidden");
+}
+
+// For automatically hiding the pageLinks when going to a small screen.
+function handleMediaQueryChange(query) {
+  const nav = document.querySelector(".pageLinks");
+  const socials = document.querySelector(".socials");
+
+  if (query.matches) {
+    nav.classList.add("hidden");
+    socials.classList.add("hidden");
+  } else {
+    nav.classList.remove("hidden");
+    socials.classList.remove("hidden");
+  }
+}
+
+const mediaQuery = window.matchMedia("(max-width: 720px)");
+mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+handleMediaQueryChange(mediaQuery);
